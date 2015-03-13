226c85623e1ec5dd15f8024a86fbcf81e6b66d76
NL33 <stkellman@gmail.com>
2015-03-13 17:48:44 -0400

    add view for individual category

--- router.js
+++ router.js
@@ -1,4 +1,5 @@
 Auctioner.Router.map(function() { 
 	this.resource('categories', {path: '/'});  //categories will be home page
 	this.resource('new-category');
+	this.resource('category', {path: 'categories/:category_id'});   
 });
---
+++ routes/category.js
@@ -0,0 +1,4 @@
+Auctioner.CategoryRoute = Ember.Route.extend({
+  model: function(params){ 
+  	return this.store.find('category', params.category_id); 
+});
