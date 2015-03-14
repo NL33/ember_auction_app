313637d1f61693ba8a0feaff15b1723438aa2d4d
NL33 <stkellman@gmail.com>
2015-03-14 16:53:31 -0400

    update explanation code file

--- controllers/item.js
+++ controllers/item.js
@@ -1,2 +1,10 @@
 Auctioner.ItemController = Ember.ObjectController.extend({
+	actions: {
+    delete: function() {
+      if (confirm('Are you sure?')) {
+        this.get('model').destroyRecord();
+        this.transitionToRoute('category', category.id);
+      }
+    }
+  }
 });
